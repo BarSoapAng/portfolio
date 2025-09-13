dee1ee27b16bc2cf17ff71017912b753f910bf66
Angela <angela.zhou12345@gmail.com>
2025-09-13 16:11:30 -0400

    missed some stuff

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-const withMDX = require('@next/mdx')();
-
-const nextConfig = {
-  /* config options here */
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-};
-
-module.exports = withMDX(nextConfig);
