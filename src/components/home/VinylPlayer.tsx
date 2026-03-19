6e10c125f69e898ce0002deaac7c38c829313c44
soapyang <angela.zhou12345@gmail.com>
2026-03-19 15:16:34 -0400

    feat(home): poll spotify playback every second

--- src/components/home/VinylPlayer.tsx
+++ src/components/home/VinylPlayer.tsx
@@ -1,11 +1,12 @@
 import VinylPlayerClient from "./VinylPlayerClient";
 
+import { unstable_noStore as noStore } from "next/cache";
+
 import { getSpotifyPlaybackState } from "@lib/spotify";
 
 export default async function VinylPlayer() {
+  noStore();
+
   const playback = await getSpotifyPlaybackState();
-
-  const playerKey = `${playback.status}:${playback.track?.spotifyUrl ?? "none"}:${playback.track?.progressMs ?? 0}`;
-
-  return <VinylPlayerClient key={playerKey} playback={playback} />;
+  return <VinylPlayerClient playback={playback} />;
 }
