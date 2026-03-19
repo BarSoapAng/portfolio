1bd39807d1035a59e2c5c1b877fe8224a92920d8
soapyang <angela.zhou12345@gmail.com>
2026-03-19 03:45:37 -0400

    Fix top blog overview props compatibility

--- src/components/navigation/TopBlogOverview.tsx
+++ src/components/navigation/TopBlogOverview.tsx
@@ -1,11 +1,15 @@
-import TopBlogLink from './TopBlogLink'
-import { getTopPosts } from '@lib/blog'
-import { formatPostDate } from '@lib/blog-shared'
+import TopBlogLink from "./TopBlogLink";
+import { getTopPosts } from "@lib/blog";
+import { formatPostDate, type PostSummary } from "@lib/blog-shared";
 
-const DISPLAY_LIMIT = 5
+const DISPLAY_LIMIT = 5;
 
-export default function TopBlogOverview() {
-  const posts = getTopPosts(DISPLAY_LIMIT)
+type TopBlogOverviewProps = {
+  posts?: PostSummary[];
+};
+
+export default function TopBlogOverview({ posts }: TopBlogOverviewProps) {
+  const overviewPosts = posts ?? getTopPosts(DISPLAY_LIMIT);
 
   return (
     <div className="w-full max-w-[260px] border-2 border-[#8b6b4a] bg-[#f6d6a8] p-1 font-mono text-black">
@@ -15,7 +19,7 @@ export default function TopBlogOverview() {
           <div className="text-[11px] text-blue-600">latest published posts</div>
         </div>
 
-        {posts.map((post) => (
+        {overviewPosts.map((post) => (
           <TopBlogLink
             key={post.slug}
             dateLabel={formatPostDate(post.date)}
@@ -25,5 +29,5 @@ export default function TopBlogOverview() {
         ))}
       </div>
     </div>
-  )
+  );
 }
