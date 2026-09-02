24eb9395a30d53654e1ba25f4963c66b5ac9c97c
soapyang <angela.zhou12345@gmail.com>
2026-09-01 19:52:02 -0700

    Update content container width

--- src/app/GlobalStyle.tsx
+++ src/app/GlobalStyle.tsx
@@ -81,7 +81,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   main {
-    width: min(calc(100% - (2 * var(--space-4))), 900px);
+    width: min(900px, 80vw);
     margin-inline: auto;
     padding-block: var(--space-12);
   }
